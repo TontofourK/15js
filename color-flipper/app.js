c54b457f8297083bea6ffdb90b1cd8a0775ef570
Kapish Dahiya <kapish1233@gmail.com>
2024-02-18 17:26:32 +0530

    completed color flipper

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -4,7 +4,9 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
     //get random number between 0-3 for flipping background color
-    const randomNumber = 2; 
+    const randomNumber = Math.floor(Math.random() * colors.length); 
+    console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = color[randomNumber];
 });
